703be3def9bb1b789bf1ad6d2312b0cdfe047bc6
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2018-12-21 00:12:55 -0800

    added edge color and uv distortion

--- Color/Assets/Scripts/ColorSpread.cs
+++ Color/Assets/Scripts/ColorSpread.cs
@@ -9,14 +9,19 @@ public sealed class ColorSpread : PostProcessEffectSettings
     [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
     public FloatParameter blend = new FloatParameter { value = 0.5f };
     public FloatParameter size = new FloatParameter { value = 0.5f };
-    public FloatParameter k = new FloatParameter { value = 1.0f };
-    public FloatParameter p = new FloatParameter { value = 1.0f };
+    public FloatParameter k_color = new FloatParameter { value = 1.0f };
+    public FloatParameter p_color = new FloatParameter { value = 1.0f };
     public FloatParameter growthSpeed = new FloatParameter { value = 1.0f };
     public FloatParameter startTime = new FloatParameter { value = 0.0f };
     public Vector4Parameter clickLocation = new Vector4Parameter { value = new Vector4(0,0,0,0) };
     public TextureParameter noiseTex = new TextureParameter { value = null };
     public FloatParameter noiseSize = new FloatParameter { value = 1.0f };
     public FloatParameter noiseTexScale = new FloatParameter { value = 1.0f };
+    public ColorParameter startColor = new ColorParameter { value = Color.white };
+    public ColorParameter endColor = new ColorParameter { value = Color.white };
+    public FloatParameter wiggleStrength = new FloatParameter { value = 0.5f };
+    public FloatParameter k_wiggle = new FloatParameter { value = 1.0f };
+    public FloatParameter p_wiggle = new FloatParameter { value = 1.0f };
 }
  
 public sealed class ColorSpreadRenderer : PostProcessEffectRenderer<ColorSpread>
@@ -29,8 +34,8 @@ public sealed class ColorSpreadRenderer : PostProcessEffectRenderer<ColorSpread>
 
         sheet.properties.SetFloat("_Blend", settings.blend);
         sheet.properties.SetFloat("_MaxSize", settings.size);
-        sheet.properties.SetFloat("_K", settings.k);
-        sheet.properties.SetFloat("_P", settings.p);
+        sheet.properties.SetFloat("_k_color", settings.k_color);
+        sheet.properties.SetFloat("_p_color", settings.p_color);
         sheet.properties.SetFloat("_StartTime", settings.startTime);
         sheet.properties.SetFloat("_GrowthSpeed", settings.growthSpeed);
         sheet.properties.SetMatrix("unity_ViewToWorldMatrix", context.camera.cameraToWorldMatrix);
@@ -39,6 +44,11 @@ public sealed class ColorSpreadRenderer : PostProcessEffectRenderer<ColorSpread>
         sheet.properties.SetTexture("_NoiseTex", settings.noiseTex);
         sheet.properties.SetFloat("_NoiseSize", settings.noiseSize);
         sheet.properties.SetFloat("_NoiseTexScale", settings.noiseTexScale);
+        sheet.properties.SetColor("_StartColor", settings.startColor);
+        sheet.properties.SetColor("_EndColor", settings.endColor);
+        sheet.properties.SetFloat("_WiggleStrength", settings.wiggleStrength);
+        sheet.properties.SetFloat("_k_wiggle", settings.k_wiggle);
+        sheet.properties.SetFloat("_p_wiggle", settings.p_wiggle);
 
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
--- Color/Assets/Shaders/ColorGrowth.hlsl
+++ Color/Assets/Shaders/ColorGrowth.hlsl
@@ -13,11 +13,16 @@ float _Blend;
 float _GrowthSpeed;
 float _MaxSize;
 float _StartTime;
-float _K;
-float _P;
+float _k_color;
+float _p_color;
+float _k_wiggle;
+float _p_wiggle;
 float4 _Center;
 float _NoiseSize;
 float _NoiseTexScale;
+float4 _StartColor;
+float4 _EndColor;
+float _WiggleStrength;
 TEXTURE2D_SAMPLER2D(_NoiseTex, sampler_NoiseTex);
 // Unity properties (set in ColorSpread.cs)
 float4x4 unity_ViewToWorldMatrix;
@@ -41,7 +46,8 @@ float between (float min, float max, float t) {
 // Returns black if t is not inside min & max
 float3 BlendInRange (float t, float min, float max, float3 a, float3 b) {
     float blend = (t - min) / (max - min);
-    return between(min, max, t) * smoothstep(a, b, blend);
+    blend = clamp(blend, 0.0, 1.0);
+    return between(min, max, t) * lerp(a, b, blend);
 }
 
 float3 GetWorldFromViewPosition (VertexOutput i) {
@@ -62,10 +68,10 @@ float3 GetWorldFromViewPosition (VertexOutput i) {
 // Based off of the smoothstep() function
 // But instead of using a Hermite curve,
 // use e^x with some parameters
-float Blend(float min, float max, float x) {
-    x = (x - min)/(max-min);
+float Blend(float min, float max, float x, float k, float p) {
+    x = (x - min) / (max-min);
     x = clamp(x, 0.0, 1.0);
-    x = exp(-pow(_K*(1 - x), _P));
+    x = exp(-pow(k * (1 - x), p));
     return x;
 }
 
@@ -87,24 +93,17 @@ float4 Frag(VertexOutput i) : SV_Target
     float3 worldPos = GetWorldFromViewPosition(i);
     //float4 depthColor = float4(worldPos, 1.0); // TEST
 
-    // regular color 
-    float4 fullColor = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.screenPos);
-
-    // grayscale
-    float luminance = dot(fullColor.rgb, float3(0.2126729, 0.7151522, 0.0721750));
-    float3 greyscale = lerp(fullColor.rgb, luminance.xxx, _Blend.xxx);
-
     // calculate radius based on animation starting time & current time
     float timeElapsed = _Time.y - _StartTime;
-    float currentRadius = min(timeElapsed * _GrowthSpeed, _MaxSize);
+    float effectRadius = min(timeElapsed * _GrowthSpeed, _MaxSize);
 
     // get world-space sample position for noise texture
     float2 worldUV = worldPos.xz;
     worldUV *= _NoiseTexScale;
 
     // add noise to radius
-    float noise = SAMPLE_TEXTURE2D(_NoiseTex, sampler_NoiseTex, worldUV);
-    currentRadius -= noise * _NoiseSize;
+    float noise = SAMPLE_TEXTURE2D(_NoiseTex, sampler_NoiseTex, worldUV).r;
+    float currentRadius = effectRadius - noise * _NoiseSize;
 
     // clamp radius so we don't get weird artifacts
     currentRadius = clamp(currentRadius, 0, _MaxSize);
@@ -112,16 +111,36 @@ float4 Frag(VertexOutput i) : SV_Target
     // check if distance is inside bounds of max radius
     // choose greyscale if outside, full color if inside
     float dist = distance(_Center, worldPos);
+    float blend = Blend(0, currentRadius, dist, _k_color, _p_color);
     
     //float useColor = step(dist, currentRadius);
     //float useColor = smoothstep(currentRadius, 0, dist);
     //float3 color = useColor*fullColor + (1-useColor)*greyscale; // USE FOR TWO ABOVE
 
-    float blend = Blend(0, currentRadius, dist);
+    // wiggle screen UV sample position based on noise sample
+    // modulate wiggle strength based on radius
+    float wiggleAmt = 1.0 - Blend(0, _MaxSize, effectRadius, _k_wiggle, _p_wiggle);
+    float wiggleEdge = Blend(0, currentRadius, dist, _k_wiggle, _p_wiggle);
+    _WiggleStrength *= wiggleAmt * wiggleEdge * between(0, currentRadius, dist);
+    float noiseWiggle = lerp(-1.0, 1.0, noise);
+    float2 screenUV = i.screenPos + (_WiggleStrength * noiseWiggle);
+
+    // regular color 
+    float4 fullColor = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, screenUV);
+
+    // grayscale
+    float luminance = dot(fullColor.rgb, float3(0.2126729, 0.7151522, 0.0721750));
+    float3 greyscale = lerp(fullColor.rgb, luminance.xxx, _Blend.xxx);
+
     float3 color = (1-blend)*fullColor + blend*greyscale;
+
+    //float3 tintColor = BlendInRange(dist, 0, currentRadius, _StartColor, _EndColor);
+    float3 tintColor = lerp(_StartColor, _EndColor, blend) * between(0, currentRadius, dist);
+    color += tintColor;
     
     return float4(color, 1.0);
     //return depthColor; // TEST
+    //return float4(tintColor, 1.0); // TEST
 }
 
 #endif
